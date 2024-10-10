226ee0c7c9857800a5d35f73b1591bfb6c9e1cf7
Abdul Sattar <asattar.bscsses21@iba-suk.edu.pk>
2024-10-11 00:54:19 +0500

    added message in Sum.java

--- sum.java
+++ sum.java
@@ -4,7 +4,6 @@ class Sum {
         int num1 = 5;
         int num2 = 10;
         int sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
+        System.out.println("The sum of num1 and num2 is: " + sum);
     }
 }
-cl
